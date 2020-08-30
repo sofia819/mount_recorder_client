9696dc8fc8f9039021833d405758c0c80e65763a
Evan Monroy <evanmonroy@gmail.com>
2020-08-30 12:12:38 -0400

    Input User Frontend

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+//components
+
+import InputUser from "./components/InputUser";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <InputUser />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputUser.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+const InputUser = () => 
+{
+
+    const [username, setUsername] = useState("");
+
+    const onSubmitForm = async e =>
+    {
+        e.preventDefault();
+        try 
+        {
+            const body = { username };
+            const response = await fetch("http://localhost:5000/users", 
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+
+            window.location = "/";
+        } 
+        catch (err) 
+        {
+            console.error(err.message);
+        }
+    }
+
+    return (
+        <>
+            <h1 className="text-center mt-5">Input User</h1>
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+                <input type="text" 
+                className="form-control" 
+                value={username}
+                onChange={e => setUsername(e.target.value)} />
+                <button className="btn btn-success">Add</button>
+            </form>
+        </>
+    );
+};
+
+export default InputUser;
