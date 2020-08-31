dd69b529250f7ee225f727cde7090de2924ba05d
Evan Monroy <evanmonroy@gmail.com>
2020-08-31 13:57:41 -0400

    List and Delete Users

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 //components
 
 import InputUser from "./components/InputUser";
+import ListUsers from "./components/ListUsers";
 
 function App() {
   return (
     <>
       <div className="container">
         <InputUser />
+        <ListUsers />
       </div>
     </>
   );
