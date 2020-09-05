44f1a687ca8df55ea1a01823e856c9f4db0d1908
sofia819 <sofia819@gmail.com>
2020-09-04 21:23:21 -0400

    some refactoring on input user

--- src/components/InputUser.js
+++ src/components/InputUser.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@material-ui/core";
+import { Card, Button, TextField, Grid } from "@material-ui/core";
 import { inputUserService } from "../services/userServices";
 import {
   INPUT_USER_HEADING,
   ADD_BUTTON,
   MIN_USERNAME_LEN,
+  USERNAME_TEXT,
 } from "../utils/constants";
 import PropTypes from "prop-types";
+import "./InputUser.scss";
 
 export const InputUser = (props) => {
   const [username, setUsername] = useState("");
@@ -21,18 +23,24 @@ export const InputUser = (props) => {
   };
 
   return (
-    <>
-      <h1>{INPUT_USER_HEADING}</h1>
-      <TextField
-        label="username"
-        variant="outlined"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Button onClick={inputUser} variant="contained" color="primary">
-        {ADD_BUTTON}
-      </Button>
-    </>
+    <Card className="form">
+      <h2>{INPUT_USER_HEADING}</h2>
+      <Grid container alignItems="center">
+        <Grid item xs={4}>
+          <TextField
+            label={USERNAME_TEXT}
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <Button onClick={inputUser} variant="contained" color="primary">
+            {ADD_BUTTON}
+          </Button>
+        </Grid>
+      </Grid>
+    </Card>
   );
 };
 
